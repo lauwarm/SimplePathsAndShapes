6ca088a4e40688b02a888cd7221820ce142a6029
lauwarm <10065855+lauwarm@users.noreply.github.com>
2020-12-08 10:21:47 +0100

    Initial Commit

---
+++ SimplePathsAndShapes/SimplePathsAndShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePathsAndShapesApp.swift
+//  SimplePathsAndShapes
+//
+//  Created by Fabian on 08.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SimplePathsAndShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
